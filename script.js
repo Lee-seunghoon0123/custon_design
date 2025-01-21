2ce6b968592bff2a84c6e9180ba3658259904358
Lee-seunghoon010123 <lee-seung-hoon@naver.com>
2025-01-21 09:22:57 +0900

    1차 마무리

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded",
         let menuBtn=document.querySelector('.menuBtn')
         let menu=document.querySelector('.menu')
         let typeBtn=document.querySelector('.type')
+        let closeBtn=document.querySelector('.closeBtn')
         menuBtn.addEventListener('click',
             function(e){
                 menu.classList.toggle('open')
@@ -12,6 +13,11 @@ document.addEventListener("DOMContentLoaded",
             function(e){
                 if (e.target.tagName==='BUTTON')
                 e.target.classList.toggle('typeBtnActivation')
+            }
+        )
+        closeBtn.addEventListener('click',
+            function(e){
+                menu.classList.toggle('open')
             }
         )
     }
